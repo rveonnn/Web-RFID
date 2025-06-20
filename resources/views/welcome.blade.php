cca2d4a535d901ccb29572b1ba394015bb7b6841
RkmPs <140313382+RkmPs@users.noreply.github.com>
2025-06-20 15:51:05 +0700

    change script source

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -784,7 +784,9 @@
 
 
     <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
-    <script src="https://ep.erhj.net/script/slick.min.js"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="{{ asset('js/slick.js') }}"></script>
     <script>
         console.log('server : eh-metal');
     </script>
