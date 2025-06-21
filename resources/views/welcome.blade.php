564e60d39d8de8efad9f9d7f61f959e4179cb1f3
rveonnn <rafanjaris.adytia@gmail.com>
2025-06-21 09:21:40 +0700

    cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,8 +29,8 @@
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
 
-    <link rel="stylesheet" href="https://ep.erhj.net/css/000-76ef7fbcd36c96ebf14f981bb2bc090a.css" />
-    <link rel="stylesheet" href="https://ep.erhj.net/css/slick.css" />
+    <link rel="stylesheet" href="{{ asset('css/base.css') }}">  
+    <link rel="stylesheet" href="{{ asset('css/slick.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 
